6a908fb120841a9f26eb99b5399345f247572daa
konotopdv <konotopdv@gmail.com>
2023-06-19 15:05:28 +0200

    task 2 all

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
 
 document.body.style.backgroundColor = 'blue';
 
